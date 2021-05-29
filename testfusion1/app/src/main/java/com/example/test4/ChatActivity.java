b6483b861ce9c97dfb00f8649ebec6cd8a8b8c2d
jerros740 <jeremy.ros@edu.esiee.fr>
2021-05-29 16:54:54 +0200

    Ajout de la requête permettant d'afficher tous les groupes de conversation qui nous concerne.

--- testfusion1/app/src/main/java/com/example/test4/ChatActivity.java
+++ testfusion1/app/src/main/java/com/example/test4/ChatActivity.java
@@ -1,16 +1,30 @@
 package com.example.test4;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class ChatActivity extends AppCompatActivity {
 
     RecyclerView recyclerViewMessage;
     TextView noMessageError;
+    ProgressBar chargementConv;
 
     Utilisateur utilisateur;
 
@@ -24,24 +38,81 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_conversation);
 
         this.recyclerViewMessage = findViewById(R.id.recyclerViewConversation);
-        this.utilisateur = new Utilisateur(String.valueOf(getUserID()),getUsername(),getUserMail(),getUserPassword());
+        this.chargementConv = findViewById(R.id.progressBarConversation);
         this.noMessageError = findViewById(R.id.textViewNoMessage);
 
-        s1 = new String[1];
-        s2 = new String[1];
+        this.utilisateur = new Utilisateur(String.valueOf(getUserID()),getUsername(),getUserMail(),getUserPassword());
 
-        if(s1.length == 0)
-        {
-            noMessageError.setText("Vous n'avez aucune conversation à afficher");
-        }
 
-        s1[0] = utilisateur.username;
-        s2[0] = utilisateur.email;
-
-        Adapter adapter = new Adapter(this,s1,s2);
-        recyclerViewMessage.setAdapter(adapter);
+        afficheConversation();
 
     }
+
+    public void afficheConversation()
+    {
+        requestConversation(res -> {
+                    this.chargementConv.setVisibility(View.VISIBLE);
+                    if(res.length() != 0)
+                    {
+                        String[] line = res.split("<br>");
+
+                        int line_length = line.length;
+
+                        this.s1 = new String[line_length];
+                        this.s2 = new String[line_length];
+
+                        String[] string;
+
+                        for(int i = 0; i < line_length; i++)
+                        {
+                            string = line[i].split("\\.");
+                            this.s1[i] = string[0];
+                            this.s2[i] = string[1];
+
+                            System.out.println(s1[i]+" "+s2[i]);
+                        }
+
+                        Adapter adapter = new Adapter(this,s1,s2);
+                        recyclerViewMessage.setAdapter(adapter);
+                        this.chargementConv.setVisibility(View.INVISIBLE);
+                    }
+                    else
+                    {
+                        noMessageError.setText("Vous n'avez aucune conversation à afficher");
+                        this.chargementConv.setVisibility(View.INVISIBLE);
+                    }
+
+        });
+    }
+
+    // Requête pour récupérer toutes les conversations de l'utilisateur
+    public void requestConversation(final map.VolleyCallBack callback)
+    {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String URL = "https://db-ezpfla.000webhostapp.com/afficheConv.php?id_user="+utilisateur.id_user;
+
+        StringRequest postRequest = new StringRequest(Request.Method.GET, URL, response -> {
+            Log.i("Réponse", response);
+            callback.onSuccess(response.trim());
+        }, error -> Log.e("Réponse", error.toString()));
+
+        // Ajoute la requête dans la file
+        queue.add(postRequest);
+    }
+
+    public void newConversation(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        View view_popup = getLayoutInflater().inflate(R.layout.popup_new_conv, null);
+
+        builder.setView(view_popup);
+
+
+        builder.show();
+    }
+
 
     // Récupère le nom d'utilisateur
     public String getUsername()
